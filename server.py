8ae607b0f3ab67317f31a6186c433d416920762c
remi <remi.lejeune@platform.sh>
2022-10-12 14:59:27 +0200

    .

--- server.py
+++ server.py
@@ -4,19 +4,9 @@ import sys
 
 import flask
 import flask.json
-import gevent.pywsgi
-
-from platformshconfig import Config
 
 app = flask.Flask(__name__)
-config = Config()
-
 
 @app.route('/')
 def root():
     return flask.json.jsonify({"hello": "world"})
-
-
-if __name__ == "__main__":
-    http_server = gevent.pywsgi.WSGIServer(('127.0.0.1', int(config.port)), app)
-    http_server.serve_forever()
